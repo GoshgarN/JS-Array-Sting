c630433cb06b44fe28e64fdad22893da6d71b070
GoshgarN <goshgarsn@code.edu.az>
2023-02-17 21:35:17 +0400

    add files

---
+++ assests/JS/script.js
@@ -0,0 +1,223 @@
+"use strict"
+
+// function test(){
+//     var age = 29;
+
+//     console.log(age);
+// }
+// test();
+
+let address = "Ahmedli is a great city";
+
+// let str = "Beleave me"
+
+// console.log(address.length);
+
+
+
+// function search(str){
+//     for (let i = 0; i < str.length; i++) {
+//         // console.log(str[i])
+//         if(address [i] == "q")
+//         return true
+        
+//     }
+//     return false;
+// }
+
+// console.log(search(address) ? "Found" : "Not Found");
+
+// console.log(address.includes("Great"));
+// console.log(address.at(0));
+// console.log(address.charAt(1));
+
+
+//  document.querySelector("button").addEventListener("Keydown", function (e){
+//     if (e.keyCode == 38) {
+//         console.log("Up")
+        
+//     }else if(e.keyCode== 13){
+//         console.log("Enter")
+//     }
+//  })
+
+// console.log(address.slice(1,4));
+
+// console.log(address.substring(2,5));
+
+// console.log(address.substr(1,3));
+
+// console.log(address.replace("A", "E"));
+
+// console.log(address.replaceAll("a", "@"));
+
+// console.log(address.toLocaleLowerCase());
+
+// console.log(address.toLocaleUpperCase());
+
+// let result = address.concat(str);
+
+// console.log(result);
+
+// console.log(address.trim());
+
+// console.log(address.startsWith("A"));
+
+// let result = address.split("");
+
+// console.log(result);
+
+// const SUCCESS_MESSAGE = "Register successfully";
+
+// const ERROR_MESSAGE = "This email already used";
+
+
+// let emails = "qosqar@gmail.com, cavid@gmail.com, ramil@gmail.com";
+
+// let user = {
+//     name: "Qosqar",
+//     surname: "Narimanli",
+//     email: "qosqar@gmail.com",
+//     password: "Qosqar13"
+// }
+
+// function register(user){
+//     let emails = getemails();
+
+//     if(user.name == " "){
+//         return "Dont empty"
+//     }
+
+
+//    for (const email of emailsArray) {
+//     if(user.email == email){
+//         return ERROR_MESSAGE
+//     }
+    
+//    }
+//    return SUCCESS_MESSAGE
+// }
+
+// function getemails(){
+//     let emails = "qosqar@gmail.com, cavid@gmail.com, ramil@gmail.com";
+
+//     let emailsArray = emails.split(",")
+
+// }
+
+// console.log(register(user));
+
+
+//ARRAY
+
+// let names = ["Qosqar", "Narimanli", "Alp", "Arslan"];
+
+// console.log(names[0]);
+
+// console.log(names[names.length - 1]);
+
+// names.reverse();
+
+// names.sort();
+
+// console.log(names);
+
+// console.log(names.join(","));
+
+// console.log(names.toString());
+
+// names.push("Musk")
+
+// names.pop();
+
+// names.shift();
+
+// names.unshift("Hello");
+
+
+// console.log(names);
+
+// let nums = [1,5,10,15,20];
+
+// console.log(nums.every(m=>m>5));
+
+// nums.fill(13);
+
+// console.log(nums);
+
+// console.log(names.filter(m=>m.includes("a")));
+
+// let res = nums.find(m=>m>5);
+
+// console.log(res);
+
+// console.log(nums.findIndex(m=>m==10));
+
+// function search(searchText){
+//     const fruits = ["Banana","Orange","Apple","Mango"];
+//     let index = fruits.indexOf(searchText);
+
+//     if(index == -1){
+//         console.log("Not Found");
+//         return;
+//     }
+
+//     console.log("Success")
+// }
+
+// search("Banana")
+
+// let nums = [1,5,10,15,20,25];
+
+// console.log(nums.lastIndexOf(25));
+
+// const numbers = [10,20,30];
+
+// let result = numbers.reduce((total,current)=> (total + current),6);
+
+// console.log(result);
+
+// let nums = [1,2,3,4,5,6,7,8,9];
+
+// function findOddNums(nums){
+//     let oddArr = []
+//     for (const item of nums) {
+//         if(item%2==1){
+//             oddArr.push(item)
+
+//         }
+        
+//     }
+//     return oddArr.reduce((total,current) => total + current)
+// }
+
+// function findOddNums(nums){
+// //    let oddArr = nums.filter(m=>m%2==1);
+
+//     return nums.filter(m=>m%2==1).reduce((total,current) => total + current)
+// }
+
+// console.log(findOddNums(nums));
+
+
+
+//TASK
+
+
+
+// function reversestr(name){
+//   return name.split('').reverse().join('')
+// }
+
+// console.log(reversestr("Goshgar"));
+
+
+
+
+
+
+
+
+
+
+    
